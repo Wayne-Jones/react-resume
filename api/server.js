5e7b5246aac47ed919e7f2005000942364824257
Wayne Jones <waynejones@nyu.edu>
2020-04-05 22:32:24 -0400

    Added Babel, Got MongoDB Atlas connected to API

--- api/server.js
+++ api/server.js
@@ -1,14 +1,19 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-const cors = require('cors');
-const mysql = require('mysql');
-const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 
+
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+const hostname = process.env.DB_HOST;
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
 
 app.get('env');
 
@@ -16,16 +21,12 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
+//MongoDB Connection
+mongoose.Promise = global.Promise;
+const uri = `mongodb+srv://${username}:${password}@${hostname}`;
+mongoose.connect(uri,  {useNewUrlParser: true, useUnifiedTopology: true});
+
 app.use('/', indexRouter);
-app.use(express.static('../public'));
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'test',
-});
-
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
