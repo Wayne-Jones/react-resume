a46299a9476f845d701c9441c2dc823f2b40ec20
Wayne Jones <waynejones@nyu.edu>
2020-05-03 16:57:02 -0400

    Utilized ThemeProvider for MaterialUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,51 @@
 import React from 'react';
 import './App.css';
+import {
+  createMuiTheme, ThemeProvider, responsiveFontSizes, makeStyles,
+} from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core/';
 import Experience from './components/Experience';
 import Heading from './components/Heading';
 import Skills from './components/Skills';
 import Education from './components/Education';
 import Interests from './components/Interests';
+import Summary from './components/Summary';
+import Projects from './components/Projects';
+
+let theme = createMuiTheme({
+  headingColor: {
+    green: '#54B689',
+  },
+  body: {
+    primary: '#4f4f4f',
+    secondary: '#767676',
+  },
+  mainTitle: {
+    color: '#292929',
+    fontWeight: 'bold',
+  },
+});
+
+theme = responsiveFontSizes(theme);
+
+const useStyles = makeStyles({
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    fontFamily: '"Roboto" , "Helvetica Neue", sans-serif',
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <Interests />
+    <ThemeProvider theme={theme}>
+      <Grid container className={classes.wrapper}>
+        <Heading />
+      </Grid>
+    </ThemeProvider>
   );
 }
 
